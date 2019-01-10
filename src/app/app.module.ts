11e980535a5558630bb29efbeebc43aa96a01214
ngmt <ngmt83ngmt@gmail.com>
2019-01-10 23:34:04 +0900

    import FlexLayoutModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
