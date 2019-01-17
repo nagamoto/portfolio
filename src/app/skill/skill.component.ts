56b9baf5f31915faf747da07fd195cf5d94cd548
ngmt <ngmt83ngmt@gmail.com>
2019-01-18 00:17:31 +0900

    add skill-component

---
+++ src/app/skill/skill.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-component',
+  templateUrl: './skill.component.html',
+  styleUrls: ['./skill.component.scss']
+})
+export class SkillComponent {
+}
